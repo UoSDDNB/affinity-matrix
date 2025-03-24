0b723f301a4fa720719903b95fbd57cda5ed10db
moi-taiga <moi.t.nicholas@gmail.com>
2025-03-24 11:10:58 +0000

    Refactor plot_gene_expression_histogram to allow customizable histogram parameters

--- scripts/plot_expression.py
+++ scripts/plot_expression.py
@@ -1,4 +1,6 @@
-def plot_gene_expression_histogram(celltype_gene_matrix, output_path):
+import matplotlib.pyplot as plt
+
+def plt_gene_exp_hist(celltype_gene_matrix, output_path, xlim=30, ylim=2000, bins=100):
     """
     Plots a histogram of gene expression values from a cell type gene matrix.
     
@@ -6,9 +8,9 @@ def plot_gene_expression_histogram(celltype_gene_matrix, output_path):
     celltype_gene_matrix (pd.DataFrame): A DataFrame containing gene expression values.
     output_path (str): Path to save the generated histogram plot.
     """
-    plt.hist(celltype_gene_matrix.values.flatten(), bins=100)
-    plt.xlim(0, 30)
-    plt.ylim(0, 2000)
+    plt.hist(celltype_gene_matrix.values.flatten(), bins=bins)
+    plt.xlim(0, xlim)
+    plt.ylim(0, ylim)
     plt.title("Histogram of gene expression")
     plt.xlabel("Gene expression")
     plt.ylabel("Frequency")
