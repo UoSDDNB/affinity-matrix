47d9aa3f84a765b4726656a0f2609fe2cc897957
moi-taiga <moi.t.nicholas@gmail.com>
2025-04-02 13:54:23 +0100

    comment out UMAP

--- scripts/plot_PCA_UMAP.py
+++ scripts/plot_PCA_UMAP.py
@@ -98,84 +98,85 @@ def plot_pca(celltype_function_matrix, label_points=True):
     plt.savefig("output/figures/ELBOW_function.png")
     plt.clf()
 
-    return celltype_pca  # Return PCA-transformed data for UMAP
+    # no need to return pca, as I am not using it
+    #return celltype_pca  # Return PCA-transformed data for UMAP
 
 
-def plot_umap(pca, celltype_function_matrix, num_pcs, label_points=True):
-    """
-    Perform UMAP on PCA-transformed data and plot the UMAP projection,
-    colouring points based on predefined cell type groupings.
+# def plot_umap(pca, celltype_function_matrix, num_pcs, label_points=True):
+#     """
+#     Perform UMAP on PCA-transformed data and plot the UMAP projection,
+#     colouring points based on predefined cell type groupings.
 
-    Args:
-        pca (np.ndarray): PCA-transformed data.
-        celltype_function_matrix (pd.DataFrame): Input matrix (used for labeling).
-        num_pcs (int): Number of principal components to use for UMAP.
-        label_points (bool): Whether to label each point with the column name.
-    """
-    # Define groupings and colors
-    groupings = {
-        "Immune cells": ["macrophage", "monocyte", "T cell", "B cell",
-                         "dendritic cell", "neutrophil", "mast cell",
-                         "inflammatory cell", "myeloid cell", "plasma cell",
-                         "professional antigen presenting cell", 
-                         "alternatively activated macrophage"],
-        "Barrier and structural cells": ["epithelial cell",
-                                         "fibroblast",
-                                         "mesothelial cell", 
-                                         "smooth muscle cell",
-                                         "endothelial cell",
-                                         "chondrocyte",
-                                         "stromal cell",
-                                         "basal cell",
-                                         "mesenchymal stem cell"],
-        "Secretory cells": ["acinar cell", "goblet cell", "endocrine cell",
-                            "peptic cell", "Sertoli cell",
-                            "epithelial cell of exocrine pancreas",
-                            "thyroid follicular cell"],
-        "Contractile cells": ["ventricular cardiac muscle cell", 
-                              "cell of skeletal muscle", "smooth muscle cell"],
-        "Neural cells": ["neuron", "astrocyte", "oligodendrocyte"]
-    }
-    colors = {
-        "Immune cells": "red",
-        "Barrier and structural cells": "blue",
-        "Secretory cells": "green",
-        "Contractile cells": "purple",
-        "Neural cells": "orange",
-        "Other": "grey"  # Default color for ungrouped cell types
-    }
+#     Args:
+#         pca (np.ndarray): PCA-transformed data.
+#         celltype_function_matrix (pd.DataFrame): Input matrix (used for labeling).
+#         num_pcs (int): Number of principal components to use for UMAP.
+#         label_points (bool): Whether to label each point with the column name.
+#     """
+#     # Define groupings and colors
+#     groupings = {
+#         "Immune cells": ["macrophage", "monocyte", "T cell", "B cell",
+#                          "dendritic cell", "neutrophil", "mast cell",
+#                          "inflammatory cell", "myeloid cell", "plasma cell",
+#                          "professional antigen presenting cell", 
+#                          "alternatively activated macrophage"],
+#         "Barrier and structural cells": ["epithelial cell",
+#                                          "fibroblast",
+#                                          "mesothelial cell", 
+#                                          "smooth muscle cell",
+#                                          "endothelial cell",
+#                                          "chondrocyte",
+#                                          "stromal cell",
+#                                          "basal cell",
+#                                          "mesenchymal stem cell"],
+#         "Secretory cells": ["acinar cell", "goblet cell", "endocrine cell",
+#                             "peptic cell", "Sertoli cell",
+#                             "epithelial cell of exocrine pancreas",
+#                             "thyroid follicular cell"],
+#         "Contractile cells": ["ventricular cardiac muscle cell", 
+#                               "cell of skeletal muscle", "smooth muscle cell"],
+#         "Neural cells": ["neuron", "astrocyte", "oligodendrocyte"]
+#     }
+#     colors = {
+#         "Immune cells": "red",
+#         "Barrier and structural cells": "blue",
+#         "Secretory cells": "green",
+#         "Contractile cells": "purple",
+#         "Neural cells": "orange",
+#         "Other": "grey"  # Default color for ungrouped cell types
+#     }
 
-    # Create lookup dictionary for cell type groups
-    celltype_to_group = {celltype: group for group, celltypes in groupings.items()
-                         for celltype in celltypes}
+#     # Create lookup dictionary for cell type groups
+#     celltype_to_group = {celltype: group for group, celltypes in groupings.items()
+#                          for celltype in celltypes}
     
-    # Assign each column (cell type) to a group, defaulting to "Other"
-    celltype_groups = [celltype_to_group.get(celltype, "Other")
-                       for celltype in celltype_function_matrix.columns]
-    #celltype_colors = [colors[group] for group in celltype_groups]
+#     # Assign each column (cell type) to a group, defaulting to "Other"
+#     celltype_groups = [celltype_to_group.get(celltype, "Other")
+#                        for celltype in celltype_function_matrix.columns]
+#     #celltype_colors = [colors[group] for group in celltype_groups]
 
-    # Reduce PCA dimensions
-    pca_reduced = pca[:, :num_pcs]
+#     # Reduce PCA dimensions
+#     pca_reduced = pca[:, :num_pcs]
 
-    # UMAP
-    reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, metric='euclidean', random_state=42)
-    celltype_umap = reducer.fit_transform(pca_reduced)
+#     # UMAP
+#     reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, metric='euclidean', random_state=42)
+#     celltype_umap = reducer.fit_transform(pca_reduced)
 
-    # UMAP plot
-    plt.figure(figsize=(10, 10))
-    sns.scatterplot(x=celltype_umap[:, 0], y=celltype_umap[:, 1], hue=celltype_groups,
-                    palette=colors, alpha=0.7)
+#     # UMAP plot
+#     plt.figure(figsize=(10, 10))
+#     sns.scatterplot(x=celltype_umap[:, 0], y=celltype_umap[:, 1], hue=celltype_groups,
+#                     palette=colors, alpha=0.7)
 
-    if label_points:
-        texts = [plt.text(celltype_umap[i, 0], celltype_umap[i, 1], label, fontsize=8, alpha=0.7)
-                 for i, label in enumerate(celltype_function_matrix.columns)]
-        adjust_text(texts)
+#     if label_points:
+#         texts = [plt.text(celltype_umap[i, 0], celltype_umap[i, 1], label, fontsize=8, alpha=0.7)
+#                  for i, label in enumerate(celltype_function_matrix.columns)]
+#         adjust_text(texts)
 
-    plt.title("UMAP Projection of celltype_function_matrix")
-    plt.xlabel("UMAP 1")
-    plt.ylabel("UMAP 2")
-    plt.legend(title="Cell Groups", bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.savefig("output/figures/UMAP_function.png", bbox_inches="tight")
-    plt.clf()
+#     plt.title("UMAP Projection of celltype_function_matrix")
+#     plt.xlabel("UMAP 1")
+#     plt.ylabel("UMAP 2")
+#     plt.legend(title="Cell Groups", bbox_to_anchor=(1.05, 1), loc='upper left')
+#     plt.savefig("output/figures/UMAP_function.png", bbox_inches="tight")
+#     plt.clf()
 
 
