d556f33edb1056697ddd981fb61f83b23e2b4eb6
moi-taiga <moi.t.nicholas@gmail.com>
2025-04-02 13:45:31 +0100

    add: integrate querying functions

--- scripts/run_af_mat.py
+++ scripts/run_af_mat.py
@@ -10,8 +10,10 @@ os.chdir('/home/mtn1n22/affinity-matrix/')
 # run the functions from the functions scripts
 from scripts.build_cellType_function_mat import build_ct_fun_mat
 from scripts.build_cellType_gene_matrix import build_ct_gene_mat
-from scripts.plot_expression import plt_gene_exp_hist
 from scripts.plot_PCA_UMAP import plot_pca, plot_umap
+from scripts.query_celltype_gene_matrix import query_celltype_gene_matrix
+from scripts.query_celltype_gene_matrix_binarized import query_celltype_gene_matrix_binarized
+from scripts.query_cellType_function_matrix import query_celltype_function_matrix
 
 ### LOAD THE DATA
 adata = sc.read_h5ad('data/human_cell_landscape.h5ad')
@@ -27,20 +29,20 @@ ct_gene_mat = build_ct_gene_mat(adata)
 # ct_gene_mat.to_csv('output/celltype_gene_matrix.csv')
 # load the celltype-gene matrix as a pd dataframe
 # ct_gene_mat2 = pd.read_csv('output/celltype_gene_matrix.csv', index_col=0)
-# #check that ct_gene_mat and ct_gene_mat2 are the same
-# print(ct_gene_mat.equals(ct_gene_mat2))
-# # says not equal but the values are the same
+
+# query the celltype-gene matrix
+query_celltype_gene_matrix(ct_gene_mat)
 
 #### remove the collumn named inflamatory cell
 ####ct_gene_mat = ct_gene_mat.drop(columns=['inflammatory cell'])
 
 ### PLOT THE GENE EXPRESSION
 # run the plot_expression.py script
-plt_gene_exp_hist(ct_gene_mat,
-                  'output/figures/gene_expression_hist.png',
-                  xlim=3,
-                  ylim=8000,
-                  bins=1000)
+# plt_gene_exp_hist(ct_gene_mat,
+#                   'output/figures/gene_expression_hist.png',
+#                   xlim=3,
+#                   ylim=8000,
+#                   bins=1000)
 
 # condsider not binarizing, instead normailizing the data?
 
@@ -48,6 +50,14 @@ plt_gene_exp_hist(ct_gene_mat,
 ct_gene_mat_bin = (ct_gene_mat > 0).astype(int)
 # # save the binarized matrix to a CSV file
 # ct_gene_mat_bin.to_csv('output/celltype_gene_matrix_binarized.csv')
+# # load the binarized celltype-gene matrix as a pd dataframe
+# ct_gene_mat_bin = pd.read_csv('output/celltype_gene_matrix_binarized.csv', index_col=0)
+
+# query the celltype-gene matrix
+query_celltype_gene_matrix_binarized(ct_gene_mat_bin)
+
+## identify the genes which have a variance below the mean and remove them
+ct_gene_mat_bin = ct_gene_mat_bin[ct_gene_mat_bin.var(axis=1) >= ct_gene_mat_bin.var(axis=1).mean()]
 
 ### BUILD THE CELLTYPE FUNCTION MATRIX
 # load the go terms
@@ -62,9 +72,12 @@ ct_fun_mat = build_ct_fun_mat(ct_gene_mat_bin, gene_functions)
 # # load the celltype-function matrix as a pd dataframe
 # ct_fun_mat = pd.read_csv('output/celltype_function_matrix.csv', index_col=0)
 
-## remove functions with low variance
-# # remove functions with variance less than 2* the mean
-ct_fun_mat = ct_fun_mat[ct_fun_mat.var(axis=1) >= 2 * ct_fun_mat.var(axis=1).mean()]
+## query the celltype-function matrix
+query_celltype_function_matrix(ct_fun_mat)
+
+# ## remove functions with low variance
+# # # remove functions with variance less than 4* the mean
+# ct_fun_mat = ct_fun_mat[ct_fun_mat.var(axis=1) >= 8 * ct_fun_mat.var(axis=1).mean()]
 
 ### PCA
 # run pca on the celltype-function matrix
@@ -73,3 +86,5 @@ celltype_pca = plot_pca(ct_fun_mat)
 ### UMAP
 plot_umap(celltype_pca, ct_fun_mat, num_pcs=20)
 
+# print all celltypes
+print(ct_fun_mat.columns.tolist())
